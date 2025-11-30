8bc50e99746caa84e6fa9adebe28249c6a52a561
idan0610 <idan0610@gmail.com>
2025-11-30 20:49:10 +0200

    add some print debugs

--- src/cdcl/CdclCore.cpp
+++ src/cdcl/CdclCore.cpp
@@ -146,10 +146,10 @@ void CdclCore::notify_new_decision_level()
     CDCL_LOG(
         Stringf( "%u l%d Notified new decision level", _index, _satSolver->getLevel() ).ascii() )
 
-//    std::cout << _index << " l" << _satSolver->getLevel() << " new level ";
+    //    std::cout << _index << " l" << _satSolver->getLevel() << " new level ";
     _engine->preContextPushHook();
     pushContext();
-//    std::cout << "l" << _satSolver->getLevel() << std::endl;
+    //    std::cout << "l" << _satSolver->getLevel() << std::endl;
 
     if ( _statistics )
     {
@@ -183,8 +183,8 @@ void CdclCore::notify_backtrack( size_t new_level )
     CDCL_LOG(
         Stringf( "%u l%d Backtracking to level %d", _index, _satSolver->getLevel(), new_level )
             .ascii() )
-//    std::cout << _index << " l" << _satSolver->getLevel() << " backtrack to l" << new_level
-//              << std::endl;
+    //    std::cout << _index << " l" << _satSolver->getLevel() << " backtrack to l" << new_level
+    //              << std::endl;
 
     unsigned oldLevel = _satSolver->getLevel();
 
@@ -203,16 +203,18 @@ void CdclCore::notify_backtrack( size_t new_level )
     popContextTo( new_level );
     _engine->postContextPopHook();
 
+    std::cout << _index << " l" << _satSolver->getLevel() << " delete decision literals: ";
     for ( unsigned l = oldLevel; l > new_level; l-- )
     {
         if ( l > _decisionIndex )
             continue;
 
         ASSERT( l == _decisionIndex )
-//        std::cout << _index << " deletes decision #" << _decisionIndex << ": "
-//                  << _decisionLiterals[_decisionIndex] << std::endl;
+        ASSERT( _decisionLiterals.exists( _decisionIndex ) );
+        std::cout << "(" << _decisionIndex << "," << _decisionLiterals[_decisionIndex] << ") ";
         _decisionLiterals.erase( _decisionIndex-- );
     }
+    std::cout << std::endl;
 
     // Maintain literals to propagate learned before the decision level
     List<Pair<int, unsigned>> currentPropagations = _literalsToPropagate;
@@ -305,7 +307,7 @@ int CdclCore::cb_decide()
 
     if ( _shouldRestart )
     {
-//        std::cout << _index << " l" << _satSolver->getLevel() << " restart" << std::endl;
+        //        std::cout << _index << " l" << _satSolver->getLevel() << " restart" << std::endl;
         CDCL_LOG( Stringf( "%u l%d Should restart. Forcing backtrack to level 0.",
                            _index,
                            _satSolver->getLevel() )
@@ -337,7 +339,8 @@ int CdclCore::cb_decide()
     }
     else
     {
-//        std::cout << _index << " l" << _satSolver->getLevel() << " no decision " << std::endl;
+        //        std::cout << _index << " l" << _satSolver->getLevel() << " no decision " <<
+        //        std::endl;
         CDCL_LOG( Stringf( "%u l%d No decision made", _index, _satSolver->getLevel() ).ascii() )
         if ( _statistics )
             _statistics->incUnsignedAttribute( Statistics::NUM_SAT_SOLVER_DECISIONS );
@@ -579,9 +582,11 @@ int CdclCore::cb_add_reason_clause_lit( int propagated_lit )
                     if ( !_decisionLiterals.exists( level ) )
                     {
                         ASSERT( level == _satSolver->getLevel() )
-//                        std::cout << _index << " l" << _satSolver->getLevel()
-//                                  << " decision literal level " << level << " does not exists!"
-//                            << std::endl;
+                        //                        std::cout << _index << " l" <<
+                        //                        _satSolver->getLevel()
+                        //                                  << " decision literal level " << level
+                        //                                  << " does not exists!"
+                        //                            << std::endl;
                         continue;
                     }
 
@@ -934,8 +939,9 @@ void CdclCore::addDecisionBasedConflictClause()
     {
         if ( !_decisionLiterals.exists( l ) )
         {
-//            std::cout << _index << " level " << _satSolver->getLevel() << " decision literal #" << l
-//                << " does not exists!" << std::endl;
+            //            std::cout << _index << " level " << _satSolver->getLevel() << " decision
+            //            literal #" << l
+            //                << " does not exists!" << std::endl;
             ASSERT( l == _satSolver->getLevel() );
             continue;
         }
@@ -985,8 +991,8 @@ void CdclCore::removeLiteralFromPropagations( int literal )
 
 void CdclCore::assume( int literal )
 {
-//    std::cout << _index << " l" << _satSolver->getLevel() << " assume literal " << literal
-//              << std::endl;
+    //    std::cout << _index << " l" << _satSolver->getLevel() << " assume literal " << literal
+    //              << std::endl;
     CDCL_LOG(
         Stringf( "%u l%d Assuming literal %d", _index, _satSolver->getLevel(), literal ).ascii() )
     _satSolver->assume( literal );
@@ -1116,8 +1122,8 @@ void CdclCore::notifySingleAssignment( int lit, bool isFixed )
 
     if ( isDecision( lit ) )
     {
-//        std::cout << _index << " l" << _satSolver->getLevel() << " decision: " << lit
-//                  << " level: " << _decisionIndex + 1 << std::endl;
+        //        std::cout << _index << " l" << _satSolver->getLevel() << " decision: " << lit
+        //                  << " level: " << _decisionIndex + 1 << std::endl;
         _decisionLiterals.insert( ++_decisionIndex, lit );
     }
 
@@ -1136,10 +1142,10 @@ void CdclCore::notifySingleAssignment( int lit, bool isFixed )
         if ( !isClauseSatisfied( clause ) )
             _satisfiedClauses.insert( clause );
 
-//    if ( originalPlcPhase != PHASE_NOT_FIXED && plc->getPhaseStatus() != originalPlcPhase )
-//        std::cout << _index << " l" << _satSolver->getLevel() << " lit " << lit << " isFixed "
-//                  << isFixed << " originalPlcPhase " << originalPlcPhase
-//                  << " plc->getPhaseStatus() " << plc->getPhaseStatus() << std::endl;
+    //    if ( originalPlcPhase != PHASE_NOT_FIXED && plc->getPhaseStatus() != originalPlcPhase )
+    //        std::cout << _index << " l" << _satSolver->getLevel() << " lit " << lit << " isFixed "
+    //                  << isFixed << " originalPlcPhase " << originalPlcPhase
+    //                  << " plc->getPhaseStatus() " << plc->getPhaseStatus() << std::endl;
     ASSERT( originalPlcPhase == PHASE_NOT_FIXED || plc->getPhaseStatus() == originalPlcPhase )
 }
 
