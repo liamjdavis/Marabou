cd6707626541a8081dce6934ce0a731f5b3060c6
idan0610 <idan0610@gmail.com>
2025-12-21 10:16:31 +0200

    Revert "fix bug for conflict clause containing only snc literals"
    This reverts commit 7b42bf2cfb6814b3f99a411c7e421f8d2a044f2e.

--- src/cdcl/CdclCore.cpp
+++ src/cdcl/CdclCore.cpp
@@ -774,15 +774,12 @@ void CdclCore::addExternalClause( const Set<int> &clause, bool shareClause )
 
     _externalClauseToAdd.append( 0 );
 
-    if (clause != _sncSplitLiterals)
+    // Remove fixed literals as they are redundant
+    for ( int lit : clause )
     {
-        // Remove fixed literals as they are redundant
-        for ( int lit : clause )
-        {
-            _externalClauseToAdd.append( -lit );
-            if ( !_fixedCadicalVars.exists( lit ) && !_fixedCadicalVars.exists( -lit ) )
-                _literalToClauses[-lit].insert( _numOfClauses );
-        }
+        _externalClauseToAdd.append( -lit );
+        if ( !_fixedCadicalVars.exists( lit ) && !_fixedCadicalVars.exists( -lit ) )
+            _literalToClauses[-lit].insert( _numOfClauses );
     }
 
     ++_numOfClauses;
@@ -958,8 +955,6 @@ void CdclCore::addDecisionBasedConflictClause()
         ASSERT( _decisionLiterals.exists( l ) );
         int lit = _decisionLiterals[l];
         ASSERT( lit != 0 );
-        if (!isDecision(lit))
-            std::cout << _index << " l" << _satSolver->getLevel() << " " << lit << std::endl;
         ASSERT( isDecision( lit ) );
         if ( !_fixedCadicalVars.exists( lit ) )
             clause.insert( lit );
