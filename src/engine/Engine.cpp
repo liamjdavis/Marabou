96593e3750d65eaaf0f6f66ed6ff372facd8e4b2
idan0610 <idan0610@gmail.com>
2025-12-31 14:20:20 +0200

    minor

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -152,25 +152,15 @@ void Engine::adjustWorkMemorySize()
         throw MarabouError( MarabouError::ALLOCATION_FAILED, "Engine::work" );
 }
 
-void Engine::applySnCSplit( PiecewiseLinearCaseSplit sncSplit,
-                            String queryId,
-                            bool shouldApplySplit )
+void Engine::applySnCSplit( PiecewiseLinearCaseSplit sncSplit, String queryId )
 {
     _sncMode = true;
     _sncSplit = sncSplit;
     _queryId = queryId;
-    if ( shouldApplySplit )
-    {
-        preContextPushHook();
-        _searchTreeHandler.pushContext();
-        applySplit( sncSplit );
-        _boundManager.propagateTightenings();
-    }
-    else
-    {
-        for ( int lit : sncSplit.getCdclLiterals() )
-            _cdclCore.assume( lit );
-    }
+    preContextPushHook();
+    _searchTreeHandler.pushContext();
+    applySplit( sncSplit );
+    _boundManager.propagateTightenings();
 }
 
 bool Engine::inSnCMode() const
