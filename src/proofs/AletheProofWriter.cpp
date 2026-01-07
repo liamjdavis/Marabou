4675de43b519e280dce248475844f77f7e498e2e
OmriIsacHUJI <omri.isac@mail.huji.ac.il>
2026-01-07 16:39:04 +0200

    bugfix

--- src/proofs/AletheProofWriter.cpp
+++ src/proofs/AletheProofWriter.cpp
@@ -922,6 +922,13 @@ void AletheProofWriter::writeDelegatedLeaf( const UnsatCertificateNode *node )
 {
     String proofHole = String( "(step r_" + std::to_string( node->getId() ) ) + " (cl " +
                        getNegatedSplitsClause( getPathSplits( node ) ) + "):rule hole)\n";
+
+    List<Tightening> deps = {};
+    for (const auto &split : getPathSplits( node ))
+        for (const auto tightening : split.getBoundTightenings())
+            deps.append(tightening);
+
+    _nodeToSplits.insert(node->getId(), deps);
     _proof.append( proofHole );
 }
 
