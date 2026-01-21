79c0734898e74d36d637301b184f06edcd89daec
OmriIsacHUJI <omri.isac@mail.huji.ac.il>
2026-01-21 13:25:05 +0200

    Revert "possible inaccuracy fix"
    This reverts commit e4cb69d19d7a53504bfdb6b563dd525988441b27.

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -115,8 +115,6 @@ const unsigned GlobalConfiguration::DNC_DEPTH_THRESHOLD = 2;
 
 const double GlobalConfiguration::MINIMAL_COEFFICIENT_FOR_TIGHTENING = 0.01;
 const double GlobalConfiguration::LEMMA_CERTIFICATION_TOLERANCE = 0.00000001;
-const double GlobalConfiguration::CONTRADICTION_THRESHOLD = 0.000001;
-
 const bool GlobalConfiguration::WRITE_JSON_PROOF = false;
 const bool GlobalConfiguration::WRITE_ALETHE_PROOF = true;
 const unsigned GlobalConfiguration::ALETHE_PROOF_FLUSHING_FREQUENCY = 50;
